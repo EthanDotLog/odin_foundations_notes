fe9b08617ce05e8c95c6b05299f1631e50640f87
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2023-11-04 17:18:12 -0500

    dom testing and javascript exercises

---
+++ javascript-exercises-main/01_helloWorld/helloWorld.js
@@ -0,0 +1,10 @@
+const helloWorld = function(times word) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+};
+
+module.exports = helloWorld;
---
+++ javascript-exercises-main/02_repeatString/repeatString.js
@@ -0,0 +1,8 @@
+const repeatString = function() {
+    for (let i = 0, i < 10, i++) {
+        console.log(`i am string ${i}`)
+    }
+};
+
+// Do not edit below this line
+module.exports = repeatString;
